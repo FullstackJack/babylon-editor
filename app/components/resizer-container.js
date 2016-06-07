7de71160a7aa7241a88fc5a6794aefa61bb655d4
Kyle Bedell <kgbedell@gmail.com>
2016-06-07 16:22:50 -0700

    Update min and max panel resizer.

--- app/components/resizer-container.js
+++ app/components/resizer-container.js
@@ -6,20 +6,40 @@ export default Ember.Component.extend(ResizeAware, {
   classNameBindings: ['layout', 'resizing'],
   panels: [{title: 'Panel 1', percent: '20'}, {title: 'Panel 2', percent: '60'}, {title: 'Panel 3', percent: '20'}],
   layout: 'cols',
-  resizeStart: { x: 0, y: 0 },
   draggingHandle: -1,
+  containerSize: null,
   previousSize: null,
   dimensions: null,
+  resizeStart: { x: 0, y: 0 },
+  resizePanels: Ember.computed('draggingHandle',function(){
+    var panels = this.get('panels'),
+        index = Number.parseInt(this.get('draggingHandle')),
+        copy = [];
+
+    // Copy the percents
+    if(index >= 0){
+      console.log('Should copy.');
+      for(var i = index; i <= index + 1; i++){
+        console.log('panels', panels[i]);
+        copy[i] = {};
+        copy[i].percent = Number.parseFloat(panels[i].percent);
+      }
+    }
+    return copy;
+  }),
   resizing: Ember.computed(function(){
     if(Number.parseInt(this.get('draggingHandle')) >= 0){
       return 'resizing';
     }
   }).property('draggingHandle'),
-  containerSize: null,
   actions: {
     resizePanels: function(index, position){
+      var panels = this.get('panels');
       this.set('draggingHandle', index);
+      console.log(this.get('draggingHandle'));
       this.set('resizeStart', position);
+      console.log(panels);
+      //this.set('resizePanels', panels.slice(index, index+2));
       this.updatePanels(index, position);
     }
   },
@@ -53,23 +73,37 @@ export default Ember.Component.extend(ResizeAware, {
   updatePanels: function(index, position){
     var containerSize = this.get('containerSize'),
         panels        = this.get('panels'),
+        resizePanels  = this.get('resizePanels'),
         start,
         distance,
-        percent;
+        percent,
+        min = 1,
+        max;
 
     if(index >= 0){
+      max = resizePanels[0].percent + resizePanels[1].percent;
       start = this.get('resizeStart');
       distance = position.x - start.x;
       percent = (distance / containerSize) * 100;
 
       for(var i = index; i <= index + 1; i++){
         var val = Number.parseFloat(panels[i].percent);
-        if(i === index){
-          val += percent;
-        } else {
-          val -= percent;
-        }
-        Ember.set(panels[i], 'percent', val);
+          console.log('max',max);
+          if (i === index) {
+            var newPercent = val + percent;
+            console.log('newPercent',newPercent);
+            if(newPercent < max) {
+              console.log('New percent is less than max.');
+              val += percent;
+            }
+          } else {
+            var newPercent = val - percent;
+            if(newPercent > min) {
+              val -= percent;
+            }
+
+          }
+          Ember.set(panels[i], 'percent', val);
       }
 
       this.set('resizeStart', position);
@@ -82,7 +116,7 @@ export default Ember.Component.extend(ResizeAware, {
     if(this.get('layout') === 'cols'){
       containerSize = this.$().width();
     } else {
-      containerSize = this.$().parent().outerWidth();
+      containerSize = this.$().height();
     }
 
     this.set('containerSize', containerSize);
